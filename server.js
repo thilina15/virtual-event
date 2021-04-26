f7c78f03973cb1be00c797d694eedeb79a691ff4
thilina15 <thibbatz@gmail.com>
2021-04-26 18:48:26 +0530

    database connection

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()   
+}
+
 const express = require('express')
+const expressEjsLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const session = require('express-session')
+
 const app = express()
 
-app.get('/',(req,res)=>{
-    res.send('hellowsss')
-})
+//import routes
+const homeRoute = require('./routes/home/home')
 
-app.listen(3000)
+//app config
+app.set('view engine','ejs')
+app.set('layout', 'layouts/layout')
+app.use(expressEjsLayouts)
+app.use(bodyParser.urlencoded({limit:'10mb' , extended: false}))
+
+//databse connection
+mongoose.connect(process.env.DATABASE_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false, 
+    useCreateIndex: true}) 
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', ()=> console.log('connected to mongoose')) 
+
+app.get('/',homeRoute)
+
+app.listen(process.env.PORT||3000)
