64c42f362e43ea9df8c68e6fdf28efbfa3083584
thilina15 <thibbatz@gmail.com>
2021-05-07 15:00:05 +0530

    event admin login

--- routes/login.js
+++ routes/login.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const router = express.Router()
-const event = require('../models/event')
+const eventAdmin = require('../models/eventAdmin')
 
 //event admins and exhibitors login
 router.get('/',(req,res)=>{
     res.locals.adminLogin = false
     res.render('login/login')
 })
+
+router.post('/eventadmin',async(req,res)=>{
+    var ob = await eventAdmin.findOne({userName:req.body.userName, password:req.body.password})
+    if(ob){
+        req.session.eventAdmin=ob
+        res.send(ob)
+    }else{
+        res.redirect('/login')
+    }
+})
+
 
 //system admin login
 router.get('/admin',(req,res)=>{
